af18c8a8de64672cd6d8bb480806d8d6483f1645
roge5716 <roger.avalos@rackspace.com>
2018-05-23 15:56:39 -0500

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,33 @@
-/* global jQuery */
+/* global $ */
 /* eslint no-param-reassign: ["error", { "props": false }] */
+/* eslint no-console: 0 */
 
-(($) => {
-  $.fn.myPattern = function myPattern() {
-    const $myPattern = $(this);
-    const actions = {
-      someAction(opts) {
-        return opts.a + opts.b;
-      },
-    };
-    $.fn.myPattern.test = actions;
-    return this;
-  };
-})(jQuery);
+const kapost = {
+  globals: {
+    projectTypes: $('#project-type'),
+    deliverables: $('#deliverables'),
+  },
+  formData: null,
+  setForm(opts) {
+    const select = this.globals.projectTypes;
+    opts.forEach(o => select.append(`<option value="${o.type}">${o.type}</option>`));
+    select.on('change', (e) => {
+      const find = this.formData.find(o => o.type === e.target.value);
+      this.globals.deliverables.html('');
+      find.deliverables.forEach(d => this.globals.deliverables.append(`<option value="${d}">${d}</option>`));
+    });
+  },
+  getData() {
+    return $.getJSON('./form-options.json')
+      .done((data) => {
+        this.formData = data.projectTypes;
+        this.setForm(this.formData);
+      })
+      .fail(() => console.log('There was an issue getting the form data'));
+  },
+  init() {
+    this.getData();
+  },
+};
+
+kapost.init();
